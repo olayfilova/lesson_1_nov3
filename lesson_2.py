a9a5b9a2655c46ef3b14dd9f8f9024f58bdb638f
sales <ofilova@gmail.com>
2023-10-29 18:29:33 +0000

    adding_for dev_less_2

---
+++ lesson_2.py
@@ -0,0 +1,75 @@
+# print("hello")
+# print("hello")
+# comment
+# comment
+#""" data type"""
+# int-ціл число
+# float -2.9
+# complex (2+3k)
+# decimal - точні цифри  0.00000016101
+#str - строка
+# bool (True/False)
+# list [] список """змінні
+# tuple () кортежі незмінні
+# set {} множина """змінні
+# dict {key; value} """змінні
+# None
+
+# box = 1
+# print(box)
+# id()
+# type()
+# box = 'hello'
+# new_box = box
+# box_1 = "hello@"
+# box_2 = "hello!!"
+# print(new_box, type(new_box), id(new_box))
+# print(id(box_2))
+
+
+#####constants
+# PI =3.14
+# MONTH_IN_YEAR = 12
+# num_1 = 11
+# num_2 = 2
+# result_add = num_1 + num_2
+# result_sub = num_1 - num_2
+# result_mul = num_1 * num_2
+# result_div = num_1 / num_2
+# # result_div_2 = num_1 // num_2 #ділення до цілого числа  !!!! //
+# result_div_zalishok = num_1 % num_2 ###!!!! % zalishok vid dillennya
+# # print(result_div, type(result_div_2))
+# print(result_div_zalishok, type(result_div_zalishok))
+# result_power = num_1 ** num_2  # cтепень **
+# result_root = num_1 ** 0.5 # корень **0.5
+# print(result_power)
+
+
+# str
+# text = "hey"
+# text = '''hey "it's friday"'''
+# text = '''hey'''
+# texx = 'hey I\'m Olga'
+# text_row = "new row\n new row"
+# print(text_row)
+# text_row = r"httpp/new.me\ndoc\n.doc" ####!!!! r"" r''
+# name = "Olga Filova"
+# text_f_= f"hey I'm {name}" #####!!! f" f' все типы  встроку str
+# print(text_f_)
+# text_f_= "hey I'm" + name ###!!!! без f'   просто  + name
+# print(text_f_)
+# \ ignoring symbol
+
+
+# name = "Olga"
+# age= "30"
+# print(name + ' ' + age) ######!!!!!!!!!!!!!!!!!
+
+# name = "Olga"
+# age= 30
+# text = name + ' ' + str(age)
+# print(text)
+
+
+
+
